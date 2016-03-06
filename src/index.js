eac4de2d147887f04e620c6154a8326ef6e518ea
Benjamin Goh <masotime@gmail.com>
2016-03-06 15:57:02 -0800

    Initial work in integrating redouter

--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import methodOverride from 'method-override';
 import routes from './server/routes/index';
+import reactRoutes from './universal/react/routes/index';
+import { server } from 'redouter';
 
 const app = express();
 
 app.use('/static', express.static('./src/client'));
 app.use(bodyParser.urlencoded({extended: true})); // basic HTML form POST
 app.use(methodOverride('_method')); // because HTML, even 5, doesn't support form actions other than GET and POST
+
+// insert redouter middleware
+app.use(server.redouter({
+	rootReducer: (state /*, action */) => state, // for now
+	initialState: {},
+	routes: reactRoutes,
+	templater: (html/*, store */) => `<!doctype html>${html}`
+}));
 app.use(routes);
 
 app.listen(process.env.PORT || 3000, () => {
---
+++ src/universal/react/routes/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route } from 'react-router';
+import Template from '../pages/template';
+import { CreatePage, DeletePage, ListPage, UpdatePage, ViewPage } from '../pages/todo';
+
+export default (
+	<Route path="/" component={Template}>
+		<Route path="views/show/:id" component={ViewPage} />
+		<Route path="views/update/:id" component={UpdatePage} />
+		<Route path="views/create" component={CreatePage} />
+		<Route path="views/delete/:id" component={DeletePage} />
+		<Route path="views/list/:filter" component={ListPage} />
+	</Route>
+);
