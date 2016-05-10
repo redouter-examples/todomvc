b08122418748804b082ad098430f860253d9ba6e
Benjamin Goh <bengoh@paypal.com>
2016-05-10 08:34:35 -0700

    Fix irritating issue regarding controlled and uncontrolled components

--- src/universal/react/components/todo.js
+++ src/universal/react/components/todo.js
@@ -2,22 +2,34 @@ import React from 'react';
 import classnames from 'classnames';
 
 const Todo = React.createClass({
-	render: function () {
-		const props = this.props;
+	// all the ugly state / props sync just because React can't handle toggling between a controlled and uncontrolled checkbox
+	getInitialState() {
+		const { props } = this;
+		return { isChecked: props.status === 'DONE' }
+	},
+	componentWillReceiveProps(nextProps) {
+		this.setState({ isChecked: nextProps.status === 'DONE' });
+	},
+	onChange(e) {
+		const { props, state } = this;
+		if (props.editable) {
+			this.setState({ isChecked: !state.isChecked});
+		}
+	},
+	render() {
+		const { props, state, onChange } = this;
 		const cssClass = {
 			todo: true,
 			done: props.status === 'DONE',
 			pending: props.status === 'PENDING'
 		};
-		const isChecked = props.status === 'DONE';
 
 		return (<div className={classnames(cssClass)} data-id={props.id}>
-			<input type="checkbox" name="status" defaultChecked={isChecked} value="DONE" disabled={!props.editable} />
+			<input type="checkbox" name="status" checked={state.isChecked} onChange={onChange} disabled={!props.editable} value="DONE" />
 			{ props.editable ? <input type="hidden" name="id" value={props.id} /> : null }
 			{ props.editable ? <input type="text" name="text" defaultValue={props.text} /> : <span className="text">{props.text}</span> }
 			{ this.props.children ? <div className="actions">{this.props.children}</div> : null }
 		</div>);
-
 	}
 });
 
